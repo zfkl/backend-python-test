8a22d5dc7a3fe13d7be1b0ed1a4eaf35bccf6770
zach klok <zacharie.klok@gmail.com>
2018-03-16 15:02:33 -0700

    TASK-2-add field is_completed boolean represented by integer

--- resources/database.sql
+++ resources/database.sql
@@ -10,5 +10,6 @@ CREATE TABLE todos (
   id INTEGER PRIMARY KEY,
   user_id INT(11) NOT NULL,
   description VARCHAR(255),
+  is_completed INT(1) NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
--- resources/fixtures.sql
+++ resources/fixtures.sql
@@ -3,12 +3,12 @@ INSERT INTO users (username, password) VALUES
 ('user2', 'user2'),
 ('user3', 'user3');
 
-INSERT INTO todos (user_id, description) VALUES
-(1, 'Vivamus tempus'),
-(1, 'lorem ac odio'),
-(1, 'Ut congue odio'),
-(1, 'Sodales finibus'),
-(1, 'Accumsan nunc vitae'),
-(2, 'Lorem ipsum'),
-(2, 'In lacinia est'),
-(2, 'Odio varius gravida');
+INSERT INTO todos (user_id, description, is_completed) VALUES
+(1, 'Vivamus tempus', 0),
+(1, 'lorem ac odio', 0),
+(1, 'Ut congue odio', 0),
+(1, 'Sodales finibus', 0),
+(1, 'Accumsan nunc vitae', 0),
+(2, 'Lorem ipsum', 0),
+(2, 'In lacinia est', 0),
+(2, 'Odio varius gravida', 1);
